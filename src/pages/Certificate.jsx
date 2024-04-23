417b77008b24bc967670b920373df4514245aadb
Froxayn123 <rizkymyusuf123@gmail.com>
2024-04-23 11:57:11 +0700

    refactor: Certificates.jsx

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -4,19 +4,14 @@ import ChevronRight from "../components/ChevronRight";
 import { certificates } from "../data/certificates";
 
 const Certificate = () => {
-  const Certificates = [];
   const [curr, setCurr] = useState(0);
 
-  for (let i = 0; i < certificates.length; i++) {
-    Certificates.push(<img key={certificates[i].credentialId} src={certificates[i].img} />);
-  }
-
   const prev = () => {
-    setCurr((curr) => (curr == 0 ? Certificates.length - 1 : curr - 1));
+    setCurr((curr) => (curr == 0 ? certificates.length - 1 : curr - 1));
   };
 
   const next = () => {
-    setCurr((curr) => (curr == Certificates.length - 1 ? 0 : curr + 1));
+    setCurr((curr) => (curr == certificates.length - 1 ? 0 : curr + 1));
   };
 
   return (
@@ -33,7 +28,9 @@ const Certificate = () => {
             </button>
             <div className="flex justify-center items-center overflow-auto relative outline outline-2 outline-[#5f5390] rounded-lg">
               <div className="w-[1007px] flex transition-transform ease-out duration-500 " style={{ transform: `translateX(-${curr * 100}%)` }}>
-                {Certificates}
+                {certificates.map((certificate) => {
+                  return <img key={certificate.credentialId} src={certificate.img} />;
+                })}
               </div>
             </div>
             <button onClick={next} className="p-4 rounded-full shadow bg-[#5f5390] text-white max-md:hidden">
